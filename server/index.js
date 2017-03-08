d231a6c00875b3c8d7c84137d8e1f1c50249501e
armaango <armaan.goyal@gmail.com>
2017-03-07 23:02:00 -0800

    one way messaging tested

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const Nexmo = require('nexmo');
 
 const app = express();
 
@@ -26,5 +27,21 @@ app.post('/', (req, res) => {
   let toNumber = req.body.number;
   let text = req.body.text;
 
- // Sending SMS via Nexmo ---
+nexmo.message.sendSms(
+    18282297913, toNumber, text, {type: 'unicode'},
+    (err, responseData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.dir(responseData);
+        // Optional: add socket.io -- will explain later
+      }
+    }
+  );
 });
+
+
+const nexmo = new Nexmo({
+  apiKey: '84709e4a',
+  apiSecret: 'c3e1397c27d2547d',
+}, {debug: true});
